ae4f6b75a35c0542c05df775efc42093a42b3699
Noemi09 <al404632@uji.es>
2021-11-14 03:48:23 +0100

    Modificando de nuevo el script
    He vuelto a modificar el script porque hay que sumarle 0.1f a la posición X como dice el tutorial

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -14,7 +14,7 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         Vector2 position = transform.position;
-        position.x = position.x + 0.001f;
+        position.x = position.x + 0.1f;
         transform.position = position;
     }
 }
