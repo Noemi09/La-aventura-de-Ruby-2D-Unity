6af0a8f55d17f399519c343989df4ecfbea25486
Noemi09 <al404632@uji.es>
2021-11-14 03:38:03 +0100

    Poniendo el script en el GameObject
    Vemos como al darle al Play se mueve el personaje a la derecha. Además le modifique el script para que se moviese más despacio.

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -14,7 +14,7 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.001f;
         transform.position = position;
     }
 }
